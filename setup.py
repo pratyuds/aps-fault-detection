6ad26013741d22de1b24c821c79e16d231485942
pratyuds <pratyu.ds1117@gmail.com>
2024-01-28 05:45:59 +0530

    added few files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author = "pratyusha",
+    author_email = "pratyu.ds1117@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)   
+
